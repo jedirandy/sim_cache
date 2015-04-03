8a6d880298fb9ce12e0301f389dc2d55a07f0bc6
Sheng <jedirandy@gmail.com>
2015-04-03 00:14:44 -0400

    refactory

--- cachesim.cpp
+++ cachesim.cpp
@@ -1,22 +1,6 @@
 #include "cachesim.hpp"
 
-uint64_t g_cache_size = 0; // cache size in bytes
-uint64_t g_block_size = 0;
-uint64_t g_num_blocks = 0; // number of blocks per set
-uint64_t g_num_victim_blocks = 0;
-uint64_t g_tag_bits = 0;
-uint64_t g_tag_mask = 0;
-uint64_t g_tag_offset = 0;
-uint64_t g_index_mask = 0;
-uint64_t g_index_offset = 0;
-uint64_t g_offset_mask = 0;
-uint64_t g_index_bits = 0;
-char g_storage_policy;
-char g_replace_policy;
-
-std::map<uint64_t, CacheSet> g_sets;
-std::shared_ptr<CacheSet> g_victim;
-
+std::shared_ptr<Cache> g_cache_ptr;
 /**
  * Subroutine for initializing the cache. You many add and initialize any global or heap
  * variables as needed.
@@ -31,36 +15,21 @@ std::shared_ptr<CacheSet> g_victim;
  */
 void setup_cache(uint64_t c, uint64_t b, uint64_t s, uint64_t v, char st,
 		char r) {
-	g_cache_size = pow(2, c);
-	g_block_size = pow(2, b);
-	g_num_blocks = pow(2, s);
-	g_num_victim_blocks = pow(2, v);
-	g_storage_policy = st;
-	g_replace_policy = r;
-	g_tag_bits = 64 - c + s;
-	g_index_bits = c - b - s;
-	// update offsets
-	g_index_offset = b;
-	g_tag_offset = c - s;
-	// update masks
-	for (uint64_t i = 0; i < g_index_bits; ++i) {
-		g_index_mask |= (1 << i);
-	}
-	for (uint64_t i = 0; i < 64 - c + s; ++i) {
-		g_tag_mask |= (1 << i);
-	}
-	for (uint64_t i = 0; i < b; ++i) {
-		g_offset_mask |= (1 << i);
-	}
+	uint64_t cache_size = pow(2, c);
+	uint64_t block_size = pow(2, b);
+	uint64_t num_blocks = pow(2, s);
+	uint64_t num_victim_blocks = pow(2, v);
+	g_cache_ptr.reset(
+			new Cache(cache_size, block_size, num_blocks, num_victim_blocks, st,
+					r));
 
-	g_victim.reset(new VictimCache(g_num_victim_blocks));
-	// init cache sets
-	int num_sets = g_cache_size / (g_block_size * g_num_blocks);
-	for (int i = 0; i < num_sets; ++i) {
-		CacheSet cs(g_num_blocks, g_storage_policy, g_replace_policy, i);
-		g_sets.insert(std::make_pair(i, cs));
-	}
+	// update masks
+	uint64_t tag_bits = 64 - c + s;
+	uint64_t index_bits = c - b - s;
+	uint64_t offset_bits = b;
+	Address::init(tag_bits, index_bits, offset_bits);
 }
+
 
 /**
  * Subroutine that simulates the cache one trace event at a time.
@@ -78,26 +47,18 @@ void cache_access(char rw, uint64_t address, cache_stats_t* p_stats) {
 		p_stats->reads++;
 	}
 
-	Address adr(address);
-	auto& set = g_sets.at(adr.index);
-	if (set.access(adr)) {
-		// hit
-	} else if (g_victim->access(adr)) {
-		// miss in the main cache, hit in the victim cache
-		// 1. replace the cache in the MC by the hit cache in the VC
-		// 2. move the replaced block to the MRU position
-		auto evicted = set.add(adr);
-		g_victim->remove(adr);
-		if (evicted.first.is_valid)
-			g_victim->add(evicted.first, evicted.second);
-
+	Result result = g_cache_ptr->access(rw, address);
+	switch (result) {
+	case HIT:
+		break;
+	case HIT_VC:
 		if (rw == WRITE) {
 			p_stats->write_misses++;
 		} else {
 			p_stats->read_misses++;
 		}
-	} else {
-		// miss both
+		break;
+	case MISS:
 		if (rw == WRITE) {
 			p_stats->write_misses++;
 			p_stats->write_misses_combined++;
@@ -105,29 +66,7 @@ void cache_access(char rw, uint64_t address, cache_stats_t* p_stats) {
 			p_stats->read_misses++;
 			p_stats->read_misses_combined++;
 		}
-		if (g_storage_policy == SUBBLOCKING) {
-			// partial-miss in the MC
-			Block block = set.fetch(adr);
-			if (!block.is_null) {
-				set.add(adr, block);
-				return;
-			}
-			// if found a partial-miss block in VC
-			// 1. update it's valid bit
-			// 2. move to the main cache
-			block = g_victim->fetch(adr);
-			if (!block.is_null) {
-				block.valid |= set.which_half(adr.offset);
-				auto evicted = set.add(adr, block);
-				g_victim->remove(adr);
-				g_victim->add(evicted.first, evicted.second);
-				return;
-			}
-		}
-		// fetch from memory
-		auto evicted = set.add(adr);
-		if (evicted.first.is_valid)
-			g_victim->add(evicted.first, evicted.second);
+		break;
 	}
 }
 
@@ -139,67 +78,31 @@ void cache_access(char rw, uint64_t address, cache_stats_t* p_stats) {
  * @p_stats Pointer to the statistics structure
  */
 void complete_cache(cache_stats_t *p_stats) {
-	p_stats->misses = p_stats->write_misses_combined
-			+ p_stats->read_misses_combined;
-	p_stats->miss_rate = (double) p_stats->misses / (double) p_stats->accesses;
-
-	p_stats->hit_time = ceil(0.2 * g_num_blocks);
-	if (g_storage_policy == BLOCKING) {
-		p_stats->miss_penalty = ceil(
-				0.2 * g_num_blocks + 50 + 0.25 * g_block_size);
-	} else if (g_storage_policy == SUBBLOCKING) {
-		p_stats->miss_penalty = ceil(
-				0.2 * g_num_blocks + 50 + 0.25 * g_block_size / 2);
-	}
-
-	double misses = p_stats->misses;
-	double hits = p_stats->accesses - p_stats->misses;
-	double total = p_stats->accesses;
-	p_stats->avg_access_time = (p_stats->hit_time * hits
-			+ (p_stats->miss_penalty + p_stats->hit_time) * misses) / total;
-
-	uint64_t total_num_blocks = g_cache_size / g_block_size;
-	uint64_t overhead = 0;
-	uint64_t valid_bits = 0;
-	uint64_t controller_bits = 0;
-	uint64_t dirty_bits = 1;
-	if (g_storage_policy == BLOCKING) {
-		valid_bits = 1;
-	} else {
-		valid_bits = 2;
-	}
-	if (g_replace_policy == LRU) {
-		controller_bits = 8;
-	} else {
-		controller_bits = 4;
-	}
-	// main cache: num_block * (dirty + valid + tags + LRU controller bits)
-	overhead += total_num_blocks
-			* (dirty_bits + valid_bits + g_tag_bits + controller_bits);
-	// victim: num_blocks * (dirty + valid + tags + index + LRU controller bits)
-	overhead += g_num_victim_blocks
-			* (dirty_bits + valid_bits + g_tag_bits + g_index_bits + 8);
-
-	p_stats->storage_overhead = overhead;
-	uint64_t total_bits = 0;
-	total_bits = (total_num_blocks + g_num_victim_blocks) * g_block_size * 8;
-
-	p_stats->storage_overhead_ratio = (double) overhead / (double) total_bits;
+	g_cache_ptr->update_stats(p_stats);
 }
 
 /*
- * Address utils
+ * Address
  */
-uint64_t get_tag(uint64_t address) {
-	return g_tag_mask & (address >> g_tag_offset);
+uint64_t Address::tag_mask;
+uint64_t Address::index_mask;
+uint64_t Address::offset_mask;
+uint64_t Address::tag_offset;
+uint64_t Address::index_offset;
+uint64_t Address::tag_bits;
+uint64_t Address::index_bits;
+uint64_t Address::offset_bits;
+
+uint64_t Address::get_tag(uint64_t address) {
+	return tag_mask & (address >> tag_offset);
 }
 
-uint64_t get_index(uint64_t address) {
-	return g_index_mask & (address >> g_index_offset);
+uint64_t Address::get_index(uint64_t address) {
+	return index_mask & (address >> index_offset);
 }
 
-uint64_t get_offset(uint64_t address) {
-	return g_offset_mask & address;
+uint64_t Address::get_offset(uint64_t address) {
+	return offset_mask & address;
 }
 
 Address::Address(uint64_t address) {
@@ -209,17 +112,48 @@ Address::Address(uint64_t address) {
 	this->is_valid = true;
 }
 
-Address::Address() {
-	this->index = 0;
-	this->tag = 0;
-	this->offset = 0;
-	this->is_valid = false;
+Address::Address() :
+		tag(0), index(0), offset(0), is_valid(false) {
 }
 
+Address::Address(uint64_t tag, uint64_t index, uint64_t offset, bool is_valid) :
+		tag(tag), index(index), offset(offset), is_valid(is_valid) {
+}
+
+void Address::init(uint64_t _tag_bits, uint64_t _index_bits,
+		uint64_t _offset_bits) {
+	tag_bits = _tag_bits;
+	index_bits = _index_bits;
+	offset_bits = _offset_bits;
+	// set masks
+	for (uint64_t i = 0; i < tag_bits; ++i) {
+		tag_mask |= (1 << i);
+	}
+	for (uint64_t i = 0; i < index_bits; ++i) {
+		index_mask |= (1 << i);
+	}
+	for (uint64_t i = 0; i < offset_bits; ++i) {
+		offset_mask |= (1 << i);
+	}
+	index_offset = offset_bits;
+	tag_offset = index_bits + offset_bits;
+}
+
+Address Address::flat_address(const Address& adr) {
+	Address address(
+			((adr.tag << (tag_offset - index_offset)) | adr.index) & tag_mask,
+			adr.index, adr.offset, adr.is_valid);
+	return address;
+}
+
+uint8_t Address::which_half(uint64_t offset) {
+	if (offset < pow(2, offset_bits) / 2)
+		return 1;
+	return 2;
+}
 /*
  * Block
  */
-
 Block::Block(uint64_t tag, uint8_t valid) :
 		tag(tag), valid(valid), is_null(false) {
 }
@@ -258,7 +192,7 @@ std::pair<Address, Block> CacheSet::add(const Address& address,
 		auto iter = block_map.find(address.tag);
 		if (iter != block_map.end()) {
 			// update valid bit
-			iter->second.valid |= which_half(address.offset);
+			iter->second.valid |= Address::which_half(address.offset);
 			access(address);
 			return std::make_pair(evicted_address, new_block);
 		}
@@ -272,7 +206,7 @@ std::pair<Address, Block> CacheSet::add(const Address& address,
 		evicted_address.is_valid = true;
 	}
 	if (block.is_null) {
-		new_block = Block(address.tag, which_half(address.offset));
+		new_block = Block(address.tag, Address::which_half(address.offset));
 	} else {
 		new_block = block;
 	}
@@ -321,17 +255,17 @@ bool CacheSet::access(const Address& address) {
 	uint64_t tag = address.tag;
 #ifdef _DEBUG
 	std::cout << "accessing tag:" << address.tag << " index:" << address.index
-			<< " offset:" << address.offset << " set number:" << index;
+	<< " offset:" << address.offset << " set number:" << index;
 #endif
 	auto iter = block_map.find(tag);
 	if (iter != block_map.end()) {
 		if (storage_policy == SUBBLOCKING) {
 			auto block = iter->second;
-			if (!(block.valid & which_half(address.offset))) {
+			if (!(block.valid & Address::which_half(address.offset))) {
 #ifdef _DEBUG
 				std::cout << " Sub-blocking miss, valid:" << block.valid
-						<< " required:" << which_half(address.offset)
-						<< std::endl;
+				<< " required:" << Address::which_half(address.offset)
+				<< std::endl;
 #endif
 				// invalid half
 				return false;
@@ -370,12 +304,6 @@ Block CacheSet::fetch(const Address& address) {
 	return block;
 }
 
-uint8_t CacheSet::which_half(uint64_t offset) {
-	if (offset < g_block_size / 2)
-		return 1;
-	return 2;
-}
-
 void CacheSet::print_queue() {
 	for (auto e : block_queue) {
 		std::cout << e << ",";
@@ -390,33 +318,144 @@ VictimCache::VictimCache(uint64_t num_blocks) :
 		CacheSet(num_blocks, BLOCKING, LRU, -1) {
 }
 
-Address VictimCache::convert_address(const Address& adr) {
-	Address address;
-	address.tag = ((adr.tag << (g_tag_offset - g_index_offset)) | adr.index)
-			& g_tag_mask;
-	address.index = adr.index;
-	address.offset = adr.offset;
-	address.is_valid = adr.is_valid;
-	return address;
-}
-
 std::pair<Address, Block> VictimCache::add(const Address& adr) {
-	return CacheSet::add(convert_address(adr));
+	return CacheSet::add(Address::flat_address(adr));
 }
 
 std::pair<Address, Block> VictimCache::add(const Address& adr,
 		const Block& block) {
-	return CacheSet::add(convert_address(adr), block);
+	return CacheSet::add(Address::flat_address(adr), block);
 }
 
 int64_t VictimCache::remove(const Address& adr) {
-	return CacheSet::remove(convert_address(adr));
+	return CacheSet::remove(Address::flat_address(adr));
 }
 
 bool VictimCache::access(const Address& adr) {
-	return CacheSet::access(convert_address(adr));
+	return CacheSet::access(Address::flat_address(adr));
 }
 
 Block VictimCache::fetch(const Address& adr) {
-	return CacheSet::fetch(convert_address(adr));
+	return CacheSet::fetch(Address::flat_address(adr));
+}
+
+/*
+ * Cache
+ */
+uint64_t Cache::cache_size; // cache size in bytes
+uint64_t Cache::block_size;
+uint64_t Cache::num_blocks; // number of blocks per set
+uint64_t Cache::num_victim_blocks;
+char Cache::replace_policy;
+char Cache::storage_policy;
+
+Cache::Cache(uint64_t _cache_size, uint64_t _block_size, uint64_t _num_blocks,
+		uint64_t _num_victim_blocks, char _storage_policy,
+		char _replace_policy) {
+	cache_size = _cache_size;
+	block_size = _block_size;
+	num_blocks = _num_blocks;
+	num_victim_blocks = _num_victim_blocks;
+	storage_policy = _storage_policy;
+	replace_policy = _replace_policy;
+	// init victim cache
+	victim.reset(new VictimCache(num_victim_blocks));
+	// init cache sets
+	int num_sets = cache_size / (block_size * num_blocks);
+	for (int i = 0; i < num_sets; ++i) {
+		CacheSet cs(num_blocks, storage_policy, replace_policy, i);
+		sets.insert(std::make_pair(i, cs));
+	}
+}
+
+Result Cache::access(char rw, uint64_t address) {
+	Result result;
+	Address adr(address);
+	auto& set = sets.at(adr.index);
+	if (set.access(adr)) {
+		// hit
+		result = HIT;
+	} else if (victim->access(adr)) {
+		result = HIT_VC;
+		auto evicted = set.add(adr);
+		victim->remove(adr);
+		if (evicted.first.is_valid)
+			victim->add(evicted.first, evicted.second);
+	} else {
+		result = MISS;
+		if (storage_policy == SUBBLOCKING) {
+			// partial-miss in the MC
+			Block block = set.fetch(adr);
+			if (!block.is_null) {
+				set.add(adr, block);
+				return result;
+			}
+			// if found a partial-miss block in VC
+			// 1. update it's valid bit
+			// 2. move to the main cache
+			block = victim->fetch(adr);
+			if (!block.is_null) {
+				block.valid |= Address::which_half(adr.offset);
+				auto evicted = set.add(adr, block);
+				victim->remove(adr);
+				victim->add(evicted.first, evicted.second);
+				return result;
+			}
+		}
+		// fetch from memory
+		auto evicted = set.add(adr);
+		if (evicted.first.is_valid)
+			victim->add(evicted.first, evicted.second);
+	}
+	return result;
+}
+
+void Cache::update_stats(cache_stats_t* p_stats) {
+	p_stats->misses = p_stats->write_misses_combined
+			+ p_stats->read_misses_combined;
+	p_stats->miss_rate = (double) p_stats->misses / (double) p_stats->accesses;
+
+	p_stats->hit_time = ceil(0.2 * num_blocks);
+	if (storage_policy == BLOCKING) {
+		p_stats->miss_penalty = ceil(
+				0.2 * num_blocks + 50 + 0.25 * block_size);
+	} else if (storage_policy == SUBBLOCKING) {
+		p_stats->miss_penalty = ceil(
+				0.2 * num_blocks + 50 + 0.25 * block_size / 2);
+	}
+
+	double misses = p_stats->misses;
+	double hits = p_stats->accesses - p_stats->misses;
+	double total = p_stats->accesses;
+	p_stats->avg_access_time = (p_stats->hit_time * hits
+			+ (p_stats->miss_penalty + p_stats->hit_time) * misses) / total;
+
+	uint64_t total_num_blocks = cache_size / block_size;
+	uint64_t overhead = 0;
+	uint64_t valid_bits = 0;
+	uint64_t controller_bits = 0;
+	uint64_t dirty_bits = 1;
+	if (storage_policy == BLOCKING) {
+		valid_bits = 1;
+	} else {
+		valid_bits = 2;
+	}
+	if (replace_policy == LRU) {
+		controller_bits = 8;
+	} else {
+		controller_bits = 4;
+	}
+	// main cache: num_block * (dirty + valid + tags + LRU controller bits)
+	overhead += total_num_blocks
+			* (dirty_bits + valid_bits + Address::tag_bits + controller_bits);
+	// victim: num_blocks * (dirty + valid + tags + index + LRU controller bits)
+	overhead += num_victim_blocks
+			* (dirty_bits + valid_bits + Address::tag_bits + Address::index_bits
+					+ 8);
+
+	p_stats->storage_overhead = overhead;
+	uint64_t total_bits = 0;
+	total_bits = (total_num_blocks + num_victim_blocks) * block_size * 8;
+
+	p_stats->storage_overhead_ratio = (double) overhead / (double) total_bits;
 }
--- cachesim.hpp
+++ cachesim.hpp
@@ -15,64 +15,94 @@
 #define NONE -1
 
 struct cache_stats_t {
-    uint64_t accesses;
-    uint64_t reads;
-    uint64_t read_misses;
-    uint64_t read_misses_combined;
-    uint64_t writes;
-    uint64_t write_misses;
-    uint64_t write_misses_combined;
-    uint64_t misses;
-    uint64_t hit_time;
-    uint64_t miss_penalty;
-    double   miss_rate;
-    double   avg_access_time;
-    uint64_t storage_overhead;
-    double   storage_overhead_ratio;
+	uint64_t accesses;
+	uint64_t reads;
+	uint64_t read_misses;
+	uint64_t read_misses_combined;
+	uint64_t writes;
+	uint64_t write_misses;
+	uint64_t write_misses_combined;
+	uint64_t misses;
+	uint64_t hit_time;
+	uint64_t miss_penalty;
+	double miss_rate;
+	double avg_access_time;
+	uint64_t storage_overhead;
+	double storage_overhead_ratio;
 };
 
 void cache_access(char rw, uint64_t address, cache_stats_t* p_stats);
-void setup_cache(uint64_t c, uint64_t b, uint64_t s, uint64_t v, char st, char r);
+void setup_cache(uint64_t c, uint64_t b, uint64_t s, uint64_t v, char st,
+		char r);
 void complete_cache(cache_stats_t *p_stats);
 
-static const uint64_t DEFAULT_C = 15;   /* 32KB Cache */
-static const uint64_t DEFAULT_B = 5;    /* 32-byte blocks */
-static const uint64_t DEFAULT_S = 3;    /* 8 blocks per set */
-static const uint64_t DEFAULT_V = 2;    /* 4 victim blocks */
+static const uint64_t DEFAULT_C = 15; /* 32KB Cache */
+static const uint64_t DEFAULT_B = 5; /* 32-byte blocks */
+static const uint64_t DEFAULT_S = 3; /* 8 blocks per set */
+static const uint64_t DEFAULT_V = 2; /* 4 victim blocks */
 
-static const char     BLOCKING = 'B';
-static const char     SUBBLOCKING = 'S';
-static const char     DEFAULT_ST = BLOCKING;
+static const char BLOCKING = 'B';
+static const char SUBBLOCKING = 'S';
+static const char DEFAULT_ST = BLOCKING;
 
-static const char     LRU = 'L';
-static const char     NMRU_FIFO = 'N';
-static const char     DEFAULT_R = LRU;
+static const char LRU = 'L';
+static const char NMRU_FIFO = 'N';
+static const char DEFAULT_R = LRU;
 
 /** Argument to cache_access rw. Indicates a load */
-static const char     READ = 'r';
+static const char READ = 'r';
 /** Argument to cache_access rw. Indicates a store */
-static const char     WRITE = 'w';
+static const char WRITE = 'w';
+
+enum Result {
+	HIT,
+	HIT_VC,
+	MISS
+};
 
 /*
  * Block
  */
-class Block {
-public:
+struct Block {
 	uint64_t tag;
 	uint8_t valid;
 	bool is_null;
 	Block(uint64_t tag, uint8_t valid);
 	Block();
-	virtual ~Block(){};
+	virtual ~Block() {
+	}
 };
 
-struct Address {
+class Address {
+private:
+	static uint64_t tag_mask;
+	static uint64_t index_mask;
+	static uint64_t offset_mask;
+	static uint64_t tag_offset;
+	static uint64_t index_offset;
+
+public:
+	static uint64_t tag_bits;
+	static uint64_t index_bits;
+	static uint64_t offset_bits;
 	uint64_t tag;
 	uint64_t index;
 	uint64_t offset;
 	bool is_valid;
+
 	Address(uint64_t address);
+	Address(uint64_t tag, uint64_t index, uint64_t offset, bool is_valid);
 	Address();
+	virtual ~Address() {
+	}
+
+	static void init(uint64_t tag_bits, uint64_t index_bits,
+			uint64_t offset_bits);
+	static Address flat_address(const Address& adr); // convert to full-associative address
+	static uint64_t get_tag(uint64_t address);
+	static uint64_t get_index(uint64_t address);
+	static uint64_t get_offset(uint64_t address);
+	static uint8_t which_half(uint64_t offset); // determine which half of the sub-block from the offset
 };
 /*
  * Cache set
@@ -81,23 +111,22 @@ class CacheSet {
 protected:
 	uint64_t num_blocks;
 	std::deque<uint64_t> block_queue;
+	std::unordered_map<uint64_t, Block> block_map;
 	char replace_policy;
 	char storage_policy;
 
-
 public:
-	int count = 0;
 	int64_t index;
 	uint64_t mru_tag;
-	std::unordered_map<uint64_t, Block> block_map;
 	CacheSet(uint64_t num_blocks, char rpl_p, char sto_p, int64_t index);
-	virtual ~CacheSet(){};
+	virtual ~CacheSet() {
+	}
 	bool is_full();
-	uint8_t which_half(uint64_t offset);
 
 	// add a block from the address, return popped address and block
 	virtual std::pair<Address, Block> add(const Address& address);
-	virtual std::pair<Address, Block> add(const Address& address, const Block& block);
+	virtual std::pair<Address, Block> add(const Address& address,
+			const Block& block);
 	virtual Block fetch(const Address& address);
 	virtual int64_t remove(const Address& address);
 	virtual int64_t remove(uint64_t tag);
@@ -108,21 +137,38 @@ public:
 	size_t get_size();
 };
 
-class VictimCache: public CacheSet{
+class VictimCache: public CacheSet {
 public:
 	VictimCache(uint64_t num_blocks);
-	virtual ~VictimCache(){};
+	virtual ~VictimCache() {
+	}
 
-	Address convert_address(const Address& address);
 	virtual std::pair<Address, Block> add(const Address& address) override;
-	virtual std::pair<Address, Block> add(const Address& address, const Block& block) override;
+	virtual std::pair<Address, Block> add(const Address& address,
+			const Block& block) override;
 	virtual bool access(const Address& address) override;
 	virtual Block fetch(const Address &address) override;
 	virtual int64_t remove(const Address& address) override;
 };
 
-uint64_t get_tag(uint64_t address);
-uint64_t get_index(uint64_t address);
-uint64_t get_offset(uint64_t address);
+class Cache {
+private:
+	static uint64_t cache_size; // cache size in bytes
+	static uint64_t block_size;
+	static uint64_t num_blocks; // number of blocks per set
+	static uint64_t num_victim_blocks;
+	static char replace_policy;
+	static char storage_policy;
+	std::map<uint64_t, CacheSet> sets;
+	std::shared_ptr<CacheSet> victim;
+public:
+	Cache(uint64_t cache_size, uint64_t block_size, uint64_t num_blocks,
+			uint64_t num_victim_blocks, char storage_policy, char replace_policy);
+	virtual ~Cache() {
+	}
+
+	Result access(char rw, uint64_t address);
+	void update_stats(cache_stats_t* p_stats);
+};
 
 #endif /* CACHESIM_HPP */
